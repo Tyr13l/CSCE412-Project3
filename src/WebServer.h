29c0eaf71de0df8f0030a584633ddd34cde301ec
Alexis Santiago <amsantia@tamu.edu>
2026-02-25 23:32:24 -0600

    Create Web Server header file

---
+++ src/WebServer.h
@@ -0,0 +1,19 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+#include "Request.h"
+
+class WebServer{
+    private:
+        Request* current_request = nullptr;
+        int remaining_time = 0;
+    public:
+        WebServer();
+        ~WebServer();
+
+        void assign_request(Request* req);
+        void process_request();
+        bool is_busy() const;
+};
+
+#endif
