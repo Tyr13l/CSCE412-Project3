acaf1c62ca98d76a3630daccd4bdacc14927981b
Alexis Santiago <amsantia@tamu.edu>
2026-02-25 23:38:06 -0600

    Create Web Server cpp file

---
+++ src/WebServer.cpp
@@ -0,0 +1,30 @@
+#include "WebServer.h"
+#include <iostream>
+
+using namespace std;
+
+void WebServer::assign_request(Request* req){
+    if(current_request != nullptr){
+        cerr << "Error: WebServer is already processing a request." << endl;
+        return;
+    }
+
+    current_request = req;
+    remaining_time = req->time;
+}
+
+void WebServer::process_request(){
+    if(current_request == nullptr){
+        return;
+    }
+
+    remaining_time--;
+
+    if(remaining_time <= 0){
+        current_request = nullptr;
+    }
+}
+
+bool WebServer::is_busy() const{
+    return current_request != nullptr;
+}
